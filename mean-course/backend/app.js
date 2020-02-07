6993e9440f7232395eaf6bebea826329cd22b526
tshifhiwa <desmond_madima@yahoo.com>
2020-02-07 05:57:10 +0200

    button

--- mean-course/backend/app.js
+++ mean-course/backend/app.js
@@ -2,14 +2,23 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) =>{
-    console.log('First Middleware');
-    next();
+app.use('/api/posts',(req,res,next) =>{
+    const posts = [
+     {id: '8211245334086',
+     titile: 'this post was post by tshifhiwa',
+     content: 'this is coming form serve'
+    },
+     {id: '2242444',
+     titile: 'this new post coming',
+     content: 'i like this post'
+    },
+    ];
+    res.status(200).json({
+    message: 'post fetched succesfule',
+    posts: this.post
+});
 });
 
 
-app.use((req,res,next) =>{
-    res.send('hello form express');
-});
 
-module.export = app;
+module.exports = app;
--- mean-course/src/app/app.module.ts
+++ mean-course/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http"
 import {
   MatInputModule,
   MatCardModule,
@@ -30,7 +31,8 @@ import { PostListComponent } from './posts/posts_create/post-list/post-list.comp
     MatCardModule,
     MatButtonModule,
     MatToolbarModule,
-    MatExpansionModule
+    MatExpansionModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
